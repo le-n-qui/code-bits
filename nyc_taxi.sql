c97f32866a9aacd46b23623bda5bb3cbbefa3f1a
andy qui <30374996+le-n-qui@users.noreply.github.com>
2021-06-17 22:24:30 -0700

    Query to get hour and speed
    Hour is from pickup_datetime column and speed is calculated by dividing trip_datetime from timestamp_diff between dropoff_datetime and pickup_datetime

--- nyc_taxi.sql
+++ nyc_taxi.sql
@@ -2,3 +2,8 @@
 # Author: Google Public Dataset BigQuery team
 # Objective: Analyze 146 million NYC taxi trips to find out how fast cabs go during the day
 
+SELECT
+  EXTRACT(HOUR FROM pickup_datetime) AS hour,
+  ROUND(AVG(trip_distance / TIMESTAMP_DIFF(dropoff_datetime, pickup_datetime, SECOND))*3600, 1) AS speed
+FROM
+  `bigquery-public-data.new_york.tlc_yellow_trips_2015`
