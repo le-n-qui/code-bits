f7b8cd94696f764056129619b518f0be06f81a26
andy qui <30374996+le-n-qui@users.noreply.github.com>
2021-06-21 11:20:07 -0700

    Retrieve maximum, minimum, and average fare

--- chicago_taxi_fares.sql
+++ chicago_taxi_fares.sql
@@ -4,3 +4,11 @@
 # for rides lasting 10 minutes or longer. The results are grouped
 # by week day. It also calculates the standard deviation of fares  
 # and the total number of rides given on each day.
+SELECT
+  FORMAT('%3.2f', MAX(fare)) AS maximum_fare,
+  FORMAT('%3.2f', MIN(fare)) AS minimum_fare,
+  FORMAT('%3.2f', AVG(fare)) AS avg_fare
+FROM
+  `bigquery-public-data.chicago_taxi_trips.taxi_trips`
+WHERE
+  trip_seconds >= 600
