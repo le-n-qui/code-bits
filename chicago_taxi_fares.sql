2ac647a886061fe9a2f15b80a3892e39732db016
andy qui <30374996+le-n-qui@users.noreply.github.com>
2021-06-21 11:10:29 -0700

    Create chicago_taxi_fares.sql

---
+++ chicago_taxi_fares.sql
@@ -0,0 +1,6 @@
+#standardSQL
+# Author: Google Public Dataset BigQuery team
+# The below query retrieves the maximum, minimum, and average fare 
+# for rides lasting 10 minutes or longer. The results are grouped
+# by week day. It also calculates the standard deviation of fares  
+# and the total number of rides given on each day.
