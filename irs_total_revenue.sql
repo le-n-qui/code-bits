55470f247d61a5462eed93f9022f66da7b2f7dfe
andy qui <30374996+le-n-qui@users.noreply.github.com>
2021-06-22 22:09:30 -0700

    Retrieve total revenues from dataset

--- irs_total_revenue.sql
+++ irs_total_revenue.sql
@@ -1,3 +1,7 @@
 #standardSQL
-# Author: Public Dataset BigQuery team
+# Author: Google Public Dataset BigQuery team
 # Retrieve top 10 total revenues in IRS 990 filings for 2015
+SELECT
+  totrevenue
+FROM 
+  `bigquery-public-data.irs_990.irs_990_2015`
