605d9e4d91a9bed939c64421134d17ba3108610f
andy qui <30374996+le-n-qui@users.noreply.github.com>
2021-06-22 22:13:53 -0700

    Adding format to improve readability

--- irs_total_revenue.sql
+++ irs_total_revenue.sql
@@ -2,7 +2,7 @@
 # Author: Google Public Dataset BigQuery team
 # Retrieve top 10 total revenues in IRS 990 filings for 2015
 SELECT
-  totrevenue
+  FORMAT("%'d", totrevenue)
 FROM 
   `bigquery-public-data.irs_990.irs_990_2015`
 ORDER BY totrevenue DESC
