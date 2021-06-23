0a11b4ce325338a96d18b6f6eb776c5dd7ad879b
andy qui <30374996+le-n-qui@users.noreply.github.com>
2021-06-22 22:20:49 -0700

    Alias column name as revenue

--- irs_total_revenue.sql
+++ irs_total_revenue.sql
@@ -2,7 +2,7 @@
 # Author: Google Public Dataset BigQuery team
 # Retrieve top 10 total revenues in IRS 990 filings for 2015
 SELECT
-  FORMAT("%'d", totrevenue)
+  FORMAT("%'d", totrevenue) AS revenue
 FROM 
   `bigquery-public-data.irs_990.irs_990_2015`
 ORDER BY totrevenue DESC
