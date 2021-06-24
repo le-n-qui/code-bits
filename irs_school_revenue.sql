be8ad228cb3f88c591066927e2956d3ac8954a03
andy qui <30374996+le-n-qui@users.noreply.github.com>
2021-06-24 12:35:06 -0700

    Retrieve revenues, EINs and school status

--- irs_school_revenue.sql
+++ irs_school_revenue.sql
@@ -1,3 +1,12 @@
 #standardSQL
 # Author: Google Public Dataset BigQuery team
 # Retrieve revenues, EINs and school status for educational organizations
+SELECT
+  totrevenue AS revenue,
+  ein,
+  operateschools170cd AS is_school
+FROM
+  `bigquery-public-data.irs_990.irs_990_2015`
+ORDER BY totrevenue DESC
+LIMIT 10
+ 
