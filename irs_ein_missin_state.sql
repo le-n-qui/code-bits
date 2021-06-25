e157c496a259443ec4279312a487b8061418ce64
andy qui <30374996+le-n-qui@users.noreply.github.com>
2021-06-25 10:16:00 -0700

    Retrieve EIN, street, city, state and zip code
    Find selected values of all charities

--- irs_ein_missin_state.sql
+++ irs_ein_missin_state.sql
@@ -1,3 +1,11 @@
 #standardSQL
 # Author: Google Public Dataset BigQuery team
 # Find all values where the state is missing for the charities
+SELECT 
+  ein,
+  street,
+  city,
+  state,
+  zip
+FROM
+  `bigquery-public-data.irs_990.irs_990_ein`
