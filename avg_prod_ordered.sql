22e07c2f60ab870bd1a3b2e630e473ce4e0b4e79
andy qui <30374996+le-n-qui@users.noreply.github.com>
2021-06-26 11:44:19 -0700

    Retrieve cities, totals_transactions, visitor id
    totals_transactions are the number of products that were ordered

--- avg_prod_ordered.sql
+++ avg_prod_ordered.sql
@@ -1,3 +1,12 @@
 #standardSQL
 # Author: Google Public Dataset BigQuery team
+
+# Find cities with the highest number of products ordered
+SELECT
+  geoNetwork_city,
+  totals_transactions,
+  fullVisitorId
+FROM
+  `data-to-insights.ecommerce.rev_transactions`
+
 # Find the average number of products ordered by cities via Google Ecommerce site
