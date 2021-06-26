35606bd159fdeb32b142389e19353ef2f5799948
andy qui <30374996+le-n-qui@users.noreply.github.com>
2021-06-26 12:01:39 -0700

    Limit result of query 1; query 2 - avg num prod
    Add a new column calculating the average number of products that were ordered by cities and sort values with new column

--- avg_prod_ordered.sql
+++ avg_prod_ordered.sql
@@ -2,6 +2,7 @@
 # Author: Google Public Dataset BigQuery team
 
 # Find cities with the highest number of products ordered
+
 SELECT
   geoNetwork_city,
   SUM(totals_transactions) AS total_products_ordered,
@@ -10,5 +11,16 @@ FROM
   `data-to-insights.ecommerce.rev_transactions`
 GROUP BY geoNetwork_city
 ORDER BY total_products_ordered DESC
+LIMIT 10
 
 # Find the average number of products ordered by cities via Google Ecommerce site
+
+SELECT
+  geoNetwork_city,
+  SUM(totals_transactions) AS total_products_ordered,
+  COUNT(DISTINCT fullVisitorId) AS distinct_visitors
+  SUM(totals_transactions) / COUNT(DISTINCT fullVisitorId) AS avg_products_ordered
+FROM
+  `data-to-insights.ecommerce.rev_transactions`
+GROUP BY geoNetwork_city
+ORDER BY avg_products_ordered DESC
