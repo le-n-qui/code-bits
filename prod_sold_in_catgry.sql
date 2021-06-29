c6b8fd7efbe8632dd6c80da3c6f22ee6130bc2a6
andy qui <30374996+le-n-qui@users.noreply.github.com>
2021-06-28 22:57:31 -0700

    Currently retrieve product names and categories

--- prod_sold_in_catgry.sql
+++ prod_sold_in_catgry.sql
@@ -1,3 +1,8 @@
 #standardSQL
 # Author: Google Public Dataset BigQuery team
 # Find the number of products that were sold for each category in Google ecommerce site
+SELECT
+  hits_product_v2ProductName,
+  hits_product_v2ProductCategory
+FROM 
+  `data-to-insights.ecommerce.rev_transactions`
