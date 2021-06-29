065a96360729b188552fe25ce434a3e53c13576b
andy qui <30374996+le-n-qui@users.noreply.github.com>
2021-06-28 23:11:08 -0700

    Filter out null values
    In our product name column, there are a lot of null values that we want to remove.

--- prod_sold_in_catgry.sql
+++ prod_sold_in_catgry.sql
@@ -6,6 +6,8 @@ SELECT
   hits_product_v2ProductCategory
 FROM 
   `data-to-insights.ecommerce.rev_transactions`
+WHERE
+  hits_product_v2ProductName IS NOT NULL
 GROUP BY
   hits_product_v2ProductCategory
 ORDER BY 
