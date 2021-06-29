5e169f451a777ff023bb76733bde53b0b98981df
andy qui <30374996+le-n-qui@users.noreply.github.com>
2021-06-28 23:00:32 -0700

    Count the number of products
    We set out to count the number of product in our table. However, because we will see duplicate product names, we want to count distinct products and alias our result as the number of products.

--- prod_sold_in_catgry.sql
+++ prod_sold_in_catgry.sql
@@ -2,7 +2,7 @@
 # Author: Google Public Dataset BigQuery team
 # Find the number of products that were sold for each category in Google ecommerce site
 SELECT
-  hits_product_v2ProductName,
+  COUNT(DISTINCT hits_product_v2ProductName) AS number_of_products,
   hits_product_v2ProductCategory
 FROM 
   `data-to-insights.ecommerce.rev_transactions`
