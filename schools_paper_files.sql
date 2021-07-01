bef01e4388be40f7a5ee559ef1cc0a025c6dc4ee
andy qui <30374996+le-n-qui@users.noreply.github.com>
2021-07-01 11:46:41 -0700

    Create schools_paper_files.sql

---
+++ schools_paper_files.sql
@@ -0,0 +1,4 @@
+#standardSQL
+# Author: Google Public Dataset BigQuery team
+# Find all charities or non-profit organizations that have filed
+# IRS 990 form as schools and also filed in paper format for 2015  
