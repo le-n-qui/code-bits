001b1dbc1f69856e26ae224061e6f8cbf9642ce6
andy qui <30374996+le-n-qui@users.noreply.github.com>
2021-07-01 11:50:59 -0700

    Retrieve all values from table

--- schools_paper_files.sql
+++ schools_paper_files.sql
@@ -2,3 +2,7 @@
 # Author: Google Public Dataset BigQuery team
 # Find all charities or non-profit organizations that have filed
 # IRS 990 form as schools and also filed in paper format for 2015  
+SELECT
+  *
+FROM 
+  `bigquiery-public-data.irs_990.irs_990_2015`
