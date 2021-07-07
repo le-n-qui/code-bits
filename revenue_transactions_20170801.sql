8a561381fde83c09729b88253f70327f21028b70
andy qui <30374996+le-n-qui@users.noreply.github.com>
2021-07-06 21:23:08 -0700

    Create a new table
    This table is in the ecommerce dataset, titled revenue_transactions_20170801. We will replace the table if it already exists.

--- revenue_transactions_20170801.sql
+++ revenue_transactions_20170801.sql
@@ -1,3 +1,6 @@
 #standardSQL
 # Practice: Create a new permanent table that stores
 # all the transactions with revenue for 08/01/2017
+
+# Take one day of ecommerce data to explore
+CREATE OR REPLACE TABLE ecommerce.revenue_transactions_20170801
