695b3e4599ed9e3f427fdef29619e9d58755aeb5
andy qui <30374996+le-n-qui@users.noreply.github.com>
2021-07-06 21:29:48 -0700

    Add the third field in schema

--- revenue_transactions_20170801.sql
+++ revenue_transactions_20170801.sql
@@ -7,5 +7,6 @@ CREATE OR REPLACE TABLE ecommerce.revenue_transactions_20170801
 #schema
 (
   fullVisitorId STRING NOT NULL OPTIONS(description="Unique visitor ID"),
-  visitId STRING NOT NULL OPTIONS(description="ID of the session, not unique across all the users")
+  visitId STRING NOT NULL OPTIONS(description="ID of the session, not unique across all the users"),
+  channelGrouping STRING NOT NULL OPTIONS(description="Channel, e.g. Direct, Organic, Referral ...")
 )
