70c8168e6e0d0f66424bc2c5740e699ded1d6ed0
andy qui <30374996+le-n-qui@users.noreply.github.com>
2021-07-09 23:12:12 -0700

    Create transactions_rev_1000_or_more.sql
    Define the practice scenario and the task

---
+++ transactions_rev_1000_or_more.sql
@@ -0,0 +1,6 @@
+#standardSQL
+# Practice: An anti-fraud team has asked me to create a report
+# that lists the 10 most recent transactions that have an order
+# amount of 1,000 or more for them to review manually
+# Task: Create a new view that returns all the most recent 10
+# transactions with revenue greater than 1,000 on or after 01/01/2017
