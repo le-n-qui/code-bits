8728657e2c6aa5524b29c53e937b4caf8d5fc072
andy qui <30374996+le-n-qui@users.noreply.github.com>
2021-07-13 17:54:21 -0700

    Retrieve 4 desired fields
    Getting them from gsod of year 1929

--- merge_gsod_datasets.sql
+++ merge_gsod_datasets.sql
@@ -2,3 +2,11 @@
 # Author: Google Public Dataset BigQuery team
 # Dataset: NOAA Weather Dataset
 # Task: Vertically Merging Datasets With Same Schema
+
+SELECT
+  stn,
+  wban,
+  temp, 
+  year
+FROM
+  `bigquery-public-data.noaa_gsod.gsod1929`
