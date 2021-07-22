9a48b056335a4d12d4e602eb1980f91f33c8d862
Andy Le <andy.quile@gmail.com>
2021-07-21 23:56:46 -0700

    Now we retrieve only 4 fields from merged table to answer our query

--- data-to-insights-GCP/advanced-SQL/largest_employer_each_state_2015.sql
+++ data-to-insights-GCP/advanced-SQL/largest_employer_each_state_2015.sql
@@ -1,10 +1,13 @@
 #standardSQL
 # Largest employer per U.S. state for the 2015 filing year
 SELECT
-  *
+  ein, # Employer Identification Number
+  name, # Employer's name
+  noemployeesw3cnt AS num_of_employees,
+  state
 FROM
   `bigquery-public-data.irs_990.irs_990_2015`
     JOIN
   `bigquery-public-data.irs_990.irs_990_ein`
 USING(ein)
-LIMIT 10
+LIMIT 10;
