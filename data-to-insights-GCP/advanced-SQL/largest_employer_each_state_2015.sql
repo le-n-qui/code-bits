425186794de35966729c77c38554838411cad1fb
Andy Le <andy.quile@gmail.com>
2021-07-22 00:07:32 -0700

    Make a temporary table from our first query statement

--- data-to-insights-GCP/advanced-SQL/largest_employer_each_state_2015.sql
+++ data-to-insights-GCP/advanced-SQL/largest_employer_each_state_2015.sql
@@ -1,16 +1,15 @@
 #standardSQL
 # Largest employer per U.S. state for the 2015 filing year
+WITH employees_count_per_state AS (
 SELECT
   ein, # Employer Identification Number
   name, # Employer's name
   noemployeesw3cnt AS num_of_employees,
-  state
-
-RANKED() OVER (PARTITION BY state ORDER BY noemployeesw3cnt DESC) AS rank
-
+  state, # which state does employer reside in
+  RANKED() OVER (PARTITION BY state ORDER BY noemployeesw3cnt DESC) AS rank 
 FROM
   `bigquery-public-data.irs_990.irs_990_2015`
     JOIN
   `bigquery-public-data.irs_990.irs_990_ein`
 USING(ein)
-LIMIT 10;
+)
