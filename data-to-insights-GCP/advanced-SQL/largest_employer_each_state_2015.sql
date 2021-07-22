0f79975c869254c6d616987ebfb53e19054aacc2
Andy Le <andy.quile@gmail.com>
2021-07-21 23:53:53 -0700

    For now select all records from the merged table using ein

--- data-to-insights-GCP/advanced-SQL/largest_employer_each_state_2015.sql
+++ data-to-insights-GCP/advanced-SQL/largest_employer_each_state_2015.sql
@@ -1,2 +1,10 @@
 #standardSQL
 # Largest employer per U.S. state for the 2015 filing year
+SELECT
+  *
+FROM
+  `bigquery-public-data.irs_990.irs_990_2015`
+    JOIN
+  `bigquery-public-data.irs_990.irs_990_ein`
+USING(ein)
+LIMIT 10
