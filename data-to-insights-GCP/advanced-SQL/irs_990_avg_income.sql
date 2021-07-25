f1820bfb17a4e25d68d841b9ad2827ea91723c9e
Andy Le <andy.quile@gmail.com>
2021-07-24 23:44:45 -0700

    Retrieve count of filings, average revenue, average expenses since 2013

--- data-to-insights-GCP/advanced-SQL/irs_990_avg_income.sql
+++ data-to-insights-GCP/advanced-SQL/irs_990_avg_income.sql
@@ -5,7 +5,12 @@
 # Retrieve the year in which organization filed, the count of filings,
 # revenue and expenses since 2013
 SELECT
-  CONCAT("20", _TABLE_SUFFIX) AS year_filed
+  CONCAT("20", _TABLE_SUFFIX) AS year_filed,
+  COUNT(ein) AS nonprofit_count,
+  AVG(totrevenue) AS avg_revenue,
+  AVG(totfuncexpns) AS avg_expenses
 FROM
   `bigquery-public-data.irs_990.irs_990_20*`
 WHERE _TABLE_SUFFIX >= '13'
+GROUP BY year_filed
+ORDER BY year_filed DESC
