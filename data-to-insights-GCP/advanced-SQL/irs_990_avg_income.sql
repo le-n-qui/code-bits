6713fa527c1f78c6ca3b096e10e06e66fa55acd4
Andy Le <andy.quile@gmail.com>
2021-07-24 23:40:34 -0700

    Merge table from 2013 and later using wildcard and _TABLE_SUFFIX

--- data-to-insights-GCP/advanced-SQL/irs_990_avg_income.sql
+++ data-to-insights-GCP/advanced-SQL/irs_990_avg_income.sql
@@ -1,3 +1,11 @@
 #standardSQL
 # BigQuery Public Data: IRS 990
 # Find the average income from IRS filings in year 2013 and later
+
+# Retrieve the year in which organization filed, the count of filings,
+# revenue and expenses since 2013
+SELECT
+  CONCAT("20", _TABLE_SUFFIX) AS year_filed
+FROM
+  `bigquery-public-data.irs_990.irs_990_20*`
+WHERE _TABLE_SUFFIX >= '13'
